bdee4469b8c31176b6de32b00d40aae6768f149c
Lukas Crockett <lukas.crockett@gmail.com>
2021-03-02 16:45:17 -0700

    add temperature logging

--- datalog.py
+++ datalog.py
@@ -1,17 +1,38 @@
 #JDS6600 frequency counter datalogger
 from jds6600 import jds6600
 from time import time, strftime, sleep
+import glob
 
 
-def write_freq(freq):
-    with open("/home/lukas/Documents/tap_project/logged_frequency.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(freq)))        
+def write_freq_temp(freq):
+    with open(output, "a") as log:
+        log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(freq)),read_temp())        
 
-
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+    
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
 
 
 port = input("What is the tty number?: ")
+output = input("type path and filename: ")
 j = jds6600("/dev/tty"+port)
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
 j.measure_setcoupling("DC")
 j.measure_setgate(10)
